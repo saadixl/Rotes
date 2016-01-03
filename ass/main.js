5d5cbd528b191b2fd273799b336870ad65e761e5
saadixl <saadixl@outlook.com>
2016-01-03 12:47:10 +0600

    ui revamped

--- ass/main.js
+++ ass/main.js
@@ -1,3 +1,20 @@
+jQuery.fn.selectText = function(){
+   var doc = document;
+   var element = this[0];
+   console.log(this, element);
+   if (doc.body.createTextRange) {
+       var range = document.body.createTextRange();
+       range.moveToElementText(element);
+       range.select();
+   } else if (window.getSelection) {
+       var selection = window.getSelection();        
+       var range = document.createRange();
+       range.selectNodeContents(element);
+       selection.removeAllRanges();
+       selection.addRange(range);
+   }
+};
+
 $(document).ready(function() {
     $('.title').focus();
     if(localStorage.previousCode){
@@ -19,9 +36,19 @@ $('.content').keypress(function(e) {
         var title = $('input.title').val();
         var content = $('.content').val();
         var newTag = document.createElement("code");
-        var tagTitle = document.createTextNode(title);
-        newTag.appendChild(tagTitle);
-        newTag.setAttribute("data-tagcontent", content);
+
+        var newTagP = document.createElement("p");
+        var tagPText = document.createTextNode(title);
+        newTagP.appendChild(tagPText);
+
+        newTag.appendChild(newTagP);
+
+        var newTagSpan2 = document.createElement("span");
+        var tagSpantext2 = document.createTextNode(content);
+        newTagSpan2.appendChild(tagSpantext2);
+
+        newTag.appendChild(newTagSpan2);
+
         document.getElementById('tags').appendChild(newTag);
         $('.title, .content').val("");
         $('.title').focus();
@@ -30,9 +57,13 @@ $('.content').keypress(function(e) {
 });
 
 
-$('.tags').delegate('code', 'click', function() {
-    $('.details').val($(this).data("tagcontent")).select();
+$('.tags').delegate('p', 'click', function() {
+    $(this).parent().find("span").toggle();
 
+});
+
+$('.tags').delegate('span','click',function(){
+    $(this).selectText();
 });
 
 $('.clear').click(function(){
